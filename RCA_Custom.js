a5a83a3180b404eccc2c4c9f9e4a7d7340d70d85
iainrca <66618182+iainrca@users.noreply.github.com>
2020-06-19 16:41:02 +0100

    Update RCA_Custom.js

--- RCA_Custom.js
+++ RCA_Custom.js
@@ -3,7 +3,11 @@
 	document.getElementById("q").placeholder = " Search the Catalogue";
 	}
 	window.onload=autoFill;
-       
+
+/* Change the Behaviour of the New Search Link so that it opens Summon in existing window*/  
+
+const summonlink = "<a target='_self' href='https://rca.summon.serialssolutions.com'>New Search</a>"
+document.getElementById('Quicksearch_New Search').innerHTML = summonlink
 	
 
 
