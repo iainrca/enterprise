f214303821701409a61d5f6822d976bf51d41db4
iainrca <66618182+iainrca@users.noreply.github.com>
2020-06-19 16:43:39 +0100

    Update RCA_Custom.js

--- RCA_Custom.js
+++ RCA_Custom.js
@@ -6,8 +6,8 @@
 
 /* Change the Behaviour of the New Search Link so that it opens Summon in existing window*/  
 
-const summonlink = "<a target='_self' href='https://rca.summon.serialssolutions.com'>New Search</a>"
-document.getElementById('Quicksearch_New Search').innerHTML = summonlink
+const summonlink = "<a target='_self' href='https://rca.summon.serialssolutions.com'>New Search</a>";
+document.getElementById('Quicksearch_New Search').innerHTML = summonlink;
 	
 
 
