52724551b94566d33ef37e3d60916ce641dd2f91
iainrca <66618182+iainrca@users.noreply.github.com>
2020-07-01 11:54:26 +0100

    Create summonmsg.js

---
+++ summonmsg.js
@@ -0,0 +1,10 @@
+var startDate = new Date("July 01, 2020 00:01");
+var endDate = new Date("July 31, 2020 23:59");
+var msg = "Try the new <a href='https://rca.summon.serialssolutions.com' target='_blank'>RCA Library Search</a> to find all our resources in one place";
+
+var todayDate = new Date();
+
+if (todayDate >= startDate && todayDate <= endDate) {
+   var wrappedmsg = '<div id="systemMsg" class="systemMsg" style="padding:5px; border:1px solid #3333CC; text-align:center; background:#DFDFFF; width:100%">' + msg + '<span style="float:right; text-align:right;"></span></div>';
+   jQuery('#content').prepend(wrappedmsg);
+}
