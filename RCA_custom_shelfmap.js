89daec9d4f1e7d7d0661a11e2bd523484c5a7601
Bernard Scaife <kbwhizz1@gmail.com>
2021-10-27 20:47:23 +0100

    Update RCA_custom_shelfmap.js
    SM test with no filter on location.

--- RCA_custom_shelfmap.js
+++ RCA_custom_shelfmap.js
@@ -12,4 +12,19 @@ document.getElementById('Quicksearch_NEWSEARCH').innerHTML = summonlink;
 }
 window.onload=summonback;
 
+// ShelfMap
+$(document).ready(function() {
+  
+  var validSMLocs=["ONSHELF","REFERENCE","QR","PAMPHLET","PER_CURR","OVERSIZE","EAP","DYSLEXIA","FUELRCA","CRLCC"];
+  var pathname = window.location.pathname;
+  var bibid = pathname.match(/SD_ILS:.*[0-9]/);
+  //var Loc = $('div.asyncFieldSD_ITEM_STATUS');
+  //if(validSMLocs.indexOf(Loc) !== -1) {
+     $('tr.detailItemsTableRow td.detailItemsTable_CALLNUMBER').each(function () {
+       $( "<div id='shelfmap'><a href='https://app.shelfmap.co.uk/fp/fp?icode=44RCA&id=" + bibid + "' target='_blank' class='ShelfMap_anchor' title='Show item on ShelfMap'><img src='https://iainrca.github.io/summon/v2.svg' alt='ShelMap drop pin' height = '75px' width='75px'/>View Shelf Location</a></div></br>").insertAfter( $(this) );
+     });
+  //}
+  
+});
+
   
