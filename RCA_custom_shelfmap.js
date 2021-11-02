74c9555b4edbe92c4bf68d22d479b4b66aff1057
Bernard Scaife <kbwhizz1@gmail.com>
2021-11-02 17:49:10 +0000

    Update RCA_custom_shelfmap.js
    whats in this in the each?

--- RCA_custom_shelfmap.js
+++ RCA_custom_shelfmap.js
@@ -18,10 +18,10 @@ jQuery(document).ready(function() {
   var validSMLocs=["1:ONSHELF","1:REFERENCE","1:QR","1:PAMPHLET","1:PER_CURR","1:OVERSIZE","1:EAP","1:DYSLEXIA","1:FUELRCA","1:CRLCC"];
   var pathname = window.location.pathname;
   var bibid = pathname.match(/SD_ILS:.*[0-9]/);
-  var Loc = $('div.detailItemsTable_LOCATION');
-  console.log(Loc);
+  var Loc = $('div.detailItemsTable_LOCATION');  
   if(validSMLocs.indexOf(Loc) !== -1) {
      jQuery('tr.detailItemsTableRow td.detailItemsTable_CALLNUMBER').each(function () {
+       console.log($(this));
        jQuery( "<div id='shelfmap'><a href='https://app.shelfmap.co.uk/fp/fp?icode=44RCA&id=" + bibid + "' target='_blank' class='ShelfMap_anchor' title='Show item on ShelfMap'><img src='https://iainrca.github.io/summon/v2.svg' alt='ShelMap drop pin' height = '75px' width='75px'/>View Shelf Location</a></div></br>").insertAfter( $(this) );
      });
   }
