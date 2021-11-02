7586f8c19a30128e4d9dc4fed503cbf06e7232f4
Bernard Scaife <kbwhizz1@gmail.com>
2021-11-02 18:54:26 +0000

    Update RCA_custom_shelfmap.js

--- RCA_custom_shelfmap.js
+++ RCA_custom_shelfmap.js
@@ -18,7 +18,7 @@ jQuery(document).ready(function() {
   var validSMLocs=["1:ONSHELF","1:REFERENCE","1:QR","1:PAMPHLET","1:PER_CURR","1:OVERSIZE","1:EAP","1:DYSLEXIA","1:FUELRCA","1:CRLCC"];
   var pathname = window.location.pathname;
   var bibid = pathname.match(/SD_ILS:.*[0-9]/);
-  bibid = bibid.replace(/SD_ILS:/,'');
+  bibid = bibid.replace('SD_ILS:','');
 
   jQuery('tr.detailItemsTableRow td.detailItemsTable_CALLNUMBER').each(function () {              
        var Loc = jQuery(this).parent().find('td.detailItemsTable_LOCATION').text();    
